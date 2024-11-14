c0c785b5988ea2a336cc1cda6cd535e6dd652072
akshatarchinfotech <akshat.solanki@archinfotech.co>
2024-11-14 10:24:59 +0530

    first commit

--- cms/settings.py
+++ cms/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cms_application', 
+        'USER': 'postgres',  
+        'PASSWORD': '1234',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
