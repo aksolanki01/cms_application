fa095fb6b534212bac6b12368311dabf42aaae13
Akshat <akshat.solanki@archinfotech.co>
2024-11-15 14:38:05 +0530

    static and media changes.

--- staticfiles/js/messages.ea7478dcbae6.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const messageBox = document.getElementById('messageBox');
-
-    if (messageBox) {
-        setTimeout(() => {
-            messageBox.style.display = 'none'; // Automatically hide after 3 seconds
-        }, 3000);
-    }
-});
