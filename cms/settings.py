93262ea3a749856011ba1af31c02e87b71cf7ad7
Akshat <akshat.solanki@archinfotech.co>
2024-11-18 11:47:03 +0530

    setting with media and static change.

--- cms/settings.py
+++ cms/settings.py
@@ -141,13 +141,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collected static files
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #dependency whitenoise
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files (User-uploaded files)
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
